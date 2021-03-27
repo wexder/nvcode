29570b86befc430a6234c5782ba37e4057c82ff9
Vladimír Zahradník <vzahradnik@Vladimirs-MacBook-Pro.local>
2021-03-27 19:55:11 +0100

    fix install sh

--- utils/install.sh
+++ utils/install.sh
@@ -67,7 +67,7 @@ installcocextensions() { \
 
 cloneconfig() { \
   echo "Cloning Nvim Mach 2 configuration"
-  git clone https://github.com/ChristianChiarulli/nvim.git ~/.config/nvim
+  git clone https://github.com/wexder/nvcode.git ~/.config/nvim
 }
 
 moveoldnvim() { \
